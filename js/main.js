c73df6941eb0b1d5fa69259770d0b4db6e0e9a1e
claude00000000011-cpu <claude00000000011-cpu@users.noreply.github.com>
2026-08-09 07:08:00 +0000

    deploy: 6159368e2a93f477ef56f4208afc4e77b8c0d550

--- js/main.js
+++ js/main.js
@@ -152,7 +152,7 @@ async function doAppRefresh() {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js').catch(err => {
+    navigator.serviceWorker.register('./sw.js', { scope: './' })ter('./sw.js', { scope: './' })ter('./sw.js').catch(err => {
       console.warn('[SW] Registrazione fallita:', err);
     });
   });
