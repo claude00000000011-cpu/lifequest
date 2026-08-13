ef63aa6808c6fbfd5b01ab1cf6576bc14006d512
QuestYourLife <QuestYourLife@users.noreply.github.com>
2026-08-13 18:48:19 +0000

    deploy: ef8ec480fac2e5e8ce40d0a7c8d7fbc8e64226a0

--- js/api.js
+++ js/api.js
@@ -1273,9 +1273,14 @@ export async function syncCloudDataOnLogin(userId) {
     }
     persist();
 
-  // Sync personaggio battle + catalogo item (fire-and-forget)
+ // Sync personaggio battle (fire-and-forget)
     import('./battle/character.js').then(({ syncBattleCharacter }) => {
       syncBattleCharacter(userId);
+    });
+
+    // Precarica catalogo item al login (forceRefresh = true)
+    import('./battle/economy.js').then(({ loadItems }) => {
+      loadItems(true);
     });
 
     // Precarica il catalogo item per economy.js
